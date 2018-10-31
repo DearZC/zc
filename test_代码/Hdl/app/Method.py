d6597003694c2ff09a01e708a2b75252ef22e5b0
unknown <zcchen1314@163.com>
2018-10-31 19:21:29 +0800

    代码更新

--- test_代码/Hdl/app/Method.py
+++ test_代码/Hdl/app/Method.py
@@ -19,3 +19,42 @@ class toast():
             return True
         except :
             return False
+
+        # 获取屏幕大小
+    def get_size(self):
+        x = self.driver.get_window_size()['width']
+        y = self.driver.get_window_size()['height']
+        return (x, y)
+
+        # 向上滑动
+    def swipUp(self, t):
+        size = self.get_size()
+        self.driver.swip(size[0]*0.5, size[1]*0.75, size[0]*0.5, size[1]*0.25, t)
+
+        # 向下滑动
+    def swipDown(self, t):
+        size = self.get_size()
+        self.driver.swip(size[0]*0.5, size[1]*0.25, size[0]*0.5, size[1]*0.75, t)
+
+        # 向左滑动
+    def swipLeft(self, t):
+        size = self.get_size()
+        self.driver.swip(size[0]*0.75. size[1]*0.5, size[0]*0.25, size[1]*0.5, t)
+
+        # 向右滑动
+    def swipRight(self, t):
+        size = self.get_size()
+        self.driver.swip(size[0] * 0.25.size[1] * 0.5, size[0] * 0.75, size[1] * 0.5, t)
+
+        # 点击首页
+    def home(self):
+        self.driver.find_element_by_xpath("//*[text()='首页']").click()
+
+        # 点击社区
+    def community(self):
+        self.driver.find_element_by_xpath("//*[text()='社区']").click()
+
+        # 点击我的
+    def my(self):
+        self.driver.find_element_by_xpath("//*[text()='我的']").click()
+
