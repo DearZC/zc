8aaf5dbba3ef2d79c152d98f9383ba73bc965832
unknown <zcchen1314@163.com>
2018-11-08 16:09:22 +0800

    代码更新

--- test_代码/Hdl/bidPrice/Regression.py
+++ test_代码/Hdl/bidPrice/Regression.py
@@ -6,16 +6,28 @@ import time
 class MyTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        driver = webdriver.Chrome()
-        driver.get('http://vip.cdmbwx.com')
-        time.sleep(1)
-
-    def test_001_test(self):
-        pass
+        web = {}
+        web['platformName'] = 'Android'
+        web['deviceName'] = '79BQADSNPPXDM'
+        web['resetKeyboard'] = True
+        web['appPackage'] = 'com.android.browser'
+        # adb logcat - b events - c   清除ecents日志
+        # adb logcat - b events - s am_create_activity:*查看启动日志
+        web['appActivity'] = '.BrowserActivity'
+        web['noReset'] = True
+        web['unicodeKeyboard'] = True  # 默认输入法
+        # ROLEX['automationName'] = 'Uiautomator2'
+        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', web)
+        time.sleep(3)
+        print('打开App')
 
     @classmethod
     def tearDownClass(self):
         self.driver.quit()
+
+    def test_001_test(self):
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()
